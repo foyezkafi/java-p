df4b1b0a138c71409f223adfb2557438544a4bd5
foyezkafi <foyezkafi@gmail.com>
2025-11-14 21:26:58 +0600

    rifu

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+// const button = document.querySelector('button')
+// const h1 = document.querySelector('h1')
+
+
+// button.addEventListener('click' , ()=>{
+//     console.log('click hoise')
+//     h1.style ='color:red;'
+//     h1.innerHTML ='tasnim nur'
+
+
+
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
